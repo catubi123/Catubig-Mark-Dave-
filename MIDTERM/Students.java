d1c3b9d39bbfc95d933aebe87c9a83f6e76842c8
catubi123 <119982799+catubi123@users.noreply.github.com>
2023-10-25 13:12:04 +0800

    Add files via upload

---
+++ MIDTERM/Students.java
@@ -0,0 +1,21 @@
+class Students {
+String first name "
+String middle name" Cabingatan"; 
+String last name " Catubig";
+public void Student () {
+
+ }
+public String getfirstname() { 
+return first_name;
+}
+
+public void setMiddleName(String mName) {
+middle_name = mName;
+ }
+ public void setLastName(String lName) { 
+ last_name = lName;
+ }
+public String getFullName() {
+return first_name + " " + MIddle_name +" "  + last_name:
+ }
+ }
